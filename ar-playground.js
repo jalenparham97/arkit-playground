64523aba7adc7b687e191f944ef1162d69d60970
jalenparham97 <jalenparham97@gmail.com>
2019-10-16 16:55:55 -0400

    fixed 3d obj filename

--- ar-playground.js
+++ ar-playground.js
@@ -1,7 +1,7 @@
 const a = document.createElement("a")
 if (a.relList.supports("ar")) {
   a.setAttribute('ref', 'ar')
-  a.setAttribute('href', 'heart.hsdz')
+  a.setAttribute('href', 'heart.usdz')
   a.innerText = 'arkit button'
 
   document.querySelector('body').appendChild(a)
