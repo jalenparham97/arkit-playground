df0c46303179dea0e380ef6fffd5fba2fb5b2a1e
jalenparham97 <jalenparham97@gmail.com>
2019-10-17 11:20:52 -0400

    usdz from storage solution

--- ar-playground.js
+++ ar-playground.js
@@ -1,7 +1,7 @@
 const a = document.createElement("a")
 if (a.relList.supports("ar")) {
   a.setAttribute('ref', 'ar')
-  a.setAttribute('href', 'heart.usdz')
+  a.setAttribute('href', 'https://firebasestorage.googleapis.com/v0/b/arkit-playground.appspot.com/o/heart.usdz?alt=media&token=eaae349d-0cc7-4212-ad71-9458eece0e1c')
   a.innerText = 'arkit button'
 
   document.querySelector('body').appendChild(a)
