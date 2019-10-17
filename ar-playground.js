579d08fe9150cfff84baaaa581c682a438ff06d6
jalenparham97 <jalenparham97@gmail.com>
2019-10-17 15:30:12 -0400

    added else statement

--- ar-playground.js
+++ ar-playground.js
@@ -1,8 +1,12 @@
+const body = document.querySelector('body')
 const a = document.createElement("a")
 if (a.relList.supports("ar")) {
   a.setAttribute('ref', 'ar')
   a.setAttribute('href', 'https://firebasestorage.googleapis.com/v0/b/arkit-playground.appspot.com/o/heart.usdz?alt=media&token=eaae349d-0cc7-4212-ad71-9458eece0e1c')
   a.innerText = 'web ar viewer'
-
-  document.querySelector('body').appendChild(a)
+  body.appendChild(a)
+} else {
+  const h1 = document.createElement('h1')
+  h1.innerText = 'WebAR does not work here. Try opening the site on an iPhone device'
+  body.appendChild(h1)
 }
