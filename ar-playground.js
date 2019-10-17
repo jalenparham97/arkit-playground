4cba4f6684c44bfb15655a4ada848de716c0a21b
jalenparham97 <jalenparham97@gmail.com>
2019-10-17 12:53:23 -0400

    added some css

--- ar-playground.js
+++ ar-playground.js
@@ -2,7 +2,7 @@ const a = document.createElement("a")
 if (a.relList.supports("ar")) {
   a.setAttribute('ref', 'ar')
   a.setAttribute('href', 'https://firebasestorage.googleapis.com/v0/b/arkit-playground.appspot.com/o/heart.usdz?alt=media&token=eaae349d-0cc7-4212-ad71-9458eece0e1c')
-  a.innerText = 'arkit button'
+  a.innerText = 'web ar viewer'
 
   document.querySelector('body').appendChild(a)
-} 
+}
