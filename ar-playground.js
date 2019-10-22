1fc0c21e2a16a2b67351b85a0dd754a28d59d4f9
jalenparham97 <jalenparham97@gmail.com>
2019-10-22 14:22:46 -0400

    changled link again

--- ar-playground.js
+++ ar-playground.js
@@ -2,8 +2,7 @@ const body = document.querySelector('body')
 const a = document.createElement("a")
 if (a.relList.supports("ar")) {
   a.setAttribute('ref', 'ar')
-  // a.setAttribute('href', 'https://firebasestorage.googleapis.com/v0/b/arkit-playground.appspot.com/o/heart.usdz?alt=media&token=eaae349d-0cc7-4212-ad71-9458eece0e1c')
-  a.setAttribute('href', 'https://www.dropbox.com/sh/n2fhbrbbn2klevy/AAB4LIbXqfqtEsL4PvPOotboa?dl=0&preview=Phoebe_Leather_Chair_Arm-3912669.usdz')
+  a.setAttribute('href', 'https://firebasestorage.googleapis.com/v0/b/arkit-playground.appspot.com/o/heart.usdz?alt=media&token=eaae349d-0cc7-4212-ad71-9458eece0e1c')
   a.innerText = 'web ar viewer'
   body.appendChild(a)
 } else {
